87cef91ecb146295d80f492104b1d980cc9699a3
David Leek <david.leekaleer@student.utamu.ac.ug>
2026-08-07 16:06:11 +0200

    feat(frontend): load finance workspace

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -12,6 +12,7 @@ import Subscriptions from './pages/Subscriptions';
 import PaymentHistory from './pages/PaymentHistory';
 import AdminUsers from './pages/AdminUsers';
 import Projects from './pages/Projects';
+import Finance from './pages/Finance';
 import Navbar from './components/layout/Navbar';
 import Sidebar from './components/layout/Sidebar';
 import ProtectedRoute from './components/common/ProtectedRoute';
