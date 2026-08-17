63a354b931390b37426a1bb62396ff38f05668d1
davelee001 <david.leekaleer@student.utamu.ac.ug>
2026-08-17 11:54:20 +0200

    feat(wells): add ML22 to OGM Mala

--- src/frontend/src/data/oilFields.ts
+++ src/frontend/src/data/oilFields.ts
@@ -52,6 +52,7 @@ const THARJAATH_WELLS: WellSpec[] = [
     { number: 1, pumpType: 'PCP', manifold: 'OGM1' },
 ];
 const MALA_WELLS: WellSpec[] = [
+    { number: 22, pumpType: 'PCP', manifold: 'OGM Mala' },
     { number: 21, pumpType: 'ESP', manifold: 'OGM Mala' },
     { number: 20, pumpType: 'PCP', manifold: 'OGM Mala' },
     { number: 19, pumpType: 'PCP', manifold: 'OGM Mala' },
